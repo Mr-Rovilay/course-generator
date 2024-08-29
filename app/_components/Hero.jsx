fa751425d4959526190d0e9fe79ab9c305512e2a
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-08-29 10:17:36 +0100

    sign-in/sign-up

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+<section className="bg-gray-50">
+  <div className="max-w-screen-xl px-4 py-32 mx-auto lg:flex lg:items-center">
+    <div className="max-w-xl mx-auto text-center">
+      <h1 className="text-3xl font-extrabold sm:text-5xl text-primary">
+       AI course Generator
+        <strong className="font-extrabold text-black sm:block"> Custom learning path by AI </strong>
+      </h1>
+
+      <p className="mt-4 sm:text-xl/relaxed">
+       Unlock personalized information with AI-driven course creation tailor your learning journey to fit our unique goas andd pace
+      </p>
+
+      <div className="flex flex-wrap justify-center gap-4 mt-8">
+        <a
+          className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-primary focus:outline-none sm:w-auto"
+          href="#"
+        >
+          Get Started
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
   )
 }
 
